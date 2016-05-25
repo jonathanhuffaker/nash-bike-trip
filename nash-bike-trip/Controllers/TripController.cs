3819d3e0e840718e9d8f5b385a3229a7e73573e3
jonathanhuffaker <jonathanhuffaker@gmail.com>
2016-05-25 17:55:48 -0500

    added first controller

---
+++ nash-bike-trip/Controllers/TripController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace nash_bike_trip.Controllers
+{
+    public class TripController : Controller
+    {
+        // GET: Trip
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
