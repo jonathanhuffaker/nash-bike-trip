fa0a270e55f3a9c8f8a952ad0be578fa02aa17f5
jonathanhuffaker <jonathanhuffaker@gmail.com>
2016-05-28 14:08:03 -0500

    Commit at end of class.  CRUD should work

---
+++ nash-bike-trip/Controllers/TripsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using nash_bike_trip.Models;
+
+namespace nash_bike_trip.Controllers
+{
+    public class TripsController : Controller
+    {
+        private TripDBContext db = new TripDBContext();
+
+        // GET: Trips
+        public ActionResult Index()
+        {
+            return View(db.Trips.ToList());
+        }
+
+        // GET: Trips/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trip trip = db.Trips.Find(id);
+            if (trip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trip);
+        }
+
+        // GET: Trips/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Trips/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Departure,Arrival,TripDate,TripNotes")] Trip trip)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Trips.Add(trip);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(trip);
+        }
+
+        // GET: Trips/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trip trip = db.Trips.Find(id);
+            if (trip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trip);
+        }
+
+        // POST: Trips/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Departure,Arrival,TripDate,TripNotes")] Trip trip)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(trip).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(trip);
+        }
+
+        // GET: Trips/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trip trip = db.Trips.Find(id);
+            if (trip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trip);
+        }
+
+        // POST: Trips/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Trip trip = db.Trips.Find(id);
+            db.Trips.Remove(trip);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- nash-bike-trip/Models/Trip.cs
+++ nash-bike-trip/Models/Trip.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace nash_bike_trip.Models
 {
     public class Trip
     {
+        public int ID { get; set; }
+        public string Departure { get; set; }
+        public string Arrival { get; set; }
+        public DateTime TripDate { get; set; }
+        public string TripNotes { get; set; }
+
+
+    }
+
+    public class TripDBContext : DbContext
+    {
+        public DbSet<Trip> Trips { get; set; }
     }
 }
