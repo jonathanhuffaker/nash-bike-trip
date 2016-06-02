9ea80280565d03902c8d06d17096052f99a4b0dc
jonathanhuffaker <jonathanhuffaker@gmail.com>
2016-06-01 20:00:03 -0500

    CRUD working for the trips.  Added unit test for first sanity test

---
+++ nash-bike-trip.Tests/TripTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nash_bike_trip.Models;
+
+namespace nash_bike_trip.Tests
+{
+    [TestClass]
+    public class TripTest
+    {
+        [TestMethod]
+        public void TripEnsureICanCreateAnInstance()
+        {
+            Trip t = new Trip();
+            Assert.IsNotNull(t);
+        }
+    }
+}
