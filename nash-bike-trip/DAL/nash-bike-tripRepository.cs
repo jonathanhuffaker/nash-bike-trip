265e505c2aa42a45de9a6171001846633333e916
jonathanhuffaker <jonathanhuffaker@gmail.com>
2016-06-08 19:45:38 -0500

    added nash-bike-tripRepository for getTripsCount, get trips, and addTrips

---
+++ nash-bike-trip/DAL/nash-bike-tripRepository.cs
@@ -0,0 +1,34 @@
+﻿using nash_bike_trip.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nash_bike_trip.DAL
+{
+    public class nash_bike_tripRepository
+    {
+        public nash_bike_tripContext context { get; set; }
+
+        public nash_bike_tripRepository()
+        {
+            context = new nash_bike_tripContext();
+        }
+
+        public int GetTripsCount()
+        {
+            return context.Trips.Count();
+        }
+
+        public List<Trip> GetTrips()
+        {
+            return context.Trips.ToList<Trip>();
+        }
+
+        public void AddTrip()
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+}
