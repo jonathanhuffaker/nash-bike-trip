1d7ad76f573b6b73f19fedd2ec449236e1285cab
jonathanhuffaker <jonathanhuffaker@gmail.com>
2016-06-16 17:09:28 -0500

    added generic view controler and trip controller calls repo.getTrips to retrieve list of trips and stash in viewbag.

---
+++ nash-bike-trip/Controllers/TripController.cs
@@ -0,0 +1,96 @@
+﻿using nash_bike_trip.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace nash_bike_trip.Controllers
+{
+    public class TripController : Controller
+    {
+        private nash_bike_tripRepository Repo = new nash_bike_tripRepository();
+
+        // GET: Trip
+        public ActionResult Index()
+        {
+            ViewBag.Trips = Repo.GetTrips();
+            return View();
+
+        }
+
+        //Get: Trip/Details/5
+
+        public ActionResult Details (int id)
+        {
+            return View();
+        }
+
+        //Get: Trip/Create
+        public ActionResult Create()
+        {
+            return View();
+        
+        }
+
+        //POST:  Trip/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                //TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //GET:  Trip/Edit/5
+        public  ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //Post: Trip/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                //TODO:  Add update logic here
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Trip/Delete/5
+        public ActionResult Delete (int id)
+        {
+            return View();
+        }
+
+        //POST: Trip/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                //TODO:  Add delete logic here
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+
+}
