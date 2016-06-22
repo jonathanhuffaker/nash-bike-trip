8e6d8fea5257f1d0ff722b5a730d1ce9895202e6
jonathanhuffaker <jonathanhuffaker@gmail.com>
2016-06-21 21:45:47 -0500

    Reseed was successful

--- nash-bike-trip/Migrations/Configuration.cs
+++ nash-bike-trip/Migrations/Configuration.cs
@@ -29,15 +29,15 @@ namespace nash_bike_trip.Migrations
             //    );
             //
 
-            //DateTime basetime = DateTime.Now;
+            DateTime basetime = DateTime.Now;
 
-            //context.Trips.AddOrUpdate(
-            //   trip => trip.DepartureTitle, // Is it in the database already?
-            //   new Trip { TripId = 1, DepartureTitle = "617 Whispering Hills Drive, Nasville,TN", ArrivalTitle = "500 Interstate Blvd S, Nashville, TN 37210", TripDate = basetime.AddDays(1), TripNotes = "Took new route to avoid as much traffic on Nolensville"},
-            //   new Trip { TripId = 2, DepartureTitle = "Ryman Auditorium", ArrivalTitle = "617 Whispering Hills Drive, Nasville,TN", TripDate = basetime.AddDays(2), TripNotes = "Long trip from downtown to Crieve Hall"},
-            //   new Trip { TripId = 3, DepartureTitle = "Bridgestone Arena", ArrivalTitle = "Craft Brewed, Franklin Pike, Nashville, TN", TripDate = basetime.AddHours(2), TripNotes = "Beers after the show. Straight shot" }
+            context.Trips.AddOrUpdate(
+               trip => trip.DepartureTitle, // Is it in the database already?
+               new Trip { TripId = 1, DepartureTitle = "617 Whispering Hills Drive, Nasville,TN", ArrivalTitle = "500 Interstate Blvd S, Nashville, TN 37210", TripDate = basetime.AddDays(1), TripNotes = "Took new route to avoid as much traffic on Nolensville" },
+               new Trip { TripId = 2, DepartureTitle = "Ryman Auditorium", ArrivalTitle = "617 Whispering Hills Drive, Nasville,TN", TripDate = basetime.AddDays(2), TripNotes = "Long trip from downtown to Crieve Hall" },
+               new Trip { TripId = 3, DepartureTitle = "Bridgestone Arena", ArrivalTitle = "Craft Brewed, Franklin Pike, Nashville, TN", TripDate = basetime.AddHours(2), TripNotes = "Beers after the show. Straight shot" }
 
-            //);
+            );
         }
     }
 }
